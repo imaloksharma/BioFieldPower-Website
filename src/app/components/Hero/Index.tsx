7a49a738872ab696e3a95a56ccfebed954a7f1a0
Praveen Kumar Gupta <pg23403@gmail.com>
2025-01-21 14:41:44 +0530

    Few update

--- src/app/components/Hero/Index.tsx
+++ src/app/components/Hero/Index.tsx
@@ -4,20 +4,23 @@ import React from 'react';
 
 export default function HeroSection() {
   return (
-    <section className="relative h-screen bg-cover m-4 bg-center" style={{ backgroundImage: 'url("img/Solar.png")' }}>
+    <section className="relative h-68 lg:h-screen p-8 mt-8 lg:m-4 bg-cover my-4 bg-center" style={{ backgroundImage: 'url("Img/Solar.png")' }}>
       {/* Semi-transparent overlay */}
       <div className="absolute inset-0 bg-white bg-opacity-50"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
-        <h1 className="text-5xl font-bold text-black-900">
-          Empowering Renewable Energy
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center -translate-y-5">
+
+        <h1 className="text-2xl lg:text-7xl font-bold text-green-900 font-style: italic">
+          Empowering 
+          Renewable 
+          Energy
         </h1>
-        <p className="mt-4 text-xl text-black-700">
-        &quot;Powering the Future Sustainably with Biomass Solutions&quot;
+        <p className="mt-4 text-xl lg:text-xl text-bold text-black-700">
+        &quot;Powering the Future Sustainably with Biomass Solutions&quot;   
         </p>
         <div className="mt-8 bg-white rounded-full p-4 shadow-lg">
-          <p className="text-green-600 text-xl font-medium">80% Energy Saved</p>
+          <p className="text-green-600 text-xl font-bold">80% Energy Saved</p>
 
         </div>
       </div>
