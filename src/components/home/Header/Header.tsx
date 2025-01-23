f8244901501f9f70df52fe1f11aa6d385b32d618
Ayush <ayushgarg678@gmail.com>
2025-01-23 16:05:49 +0530

    little bit changes

--- src/components/home/Header/Header.tsx
+++ src/components/home/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-6 py-4 bg-white shadow-lg">
       <div className="flex items-center space-x-2 text-green-700">
-        <img className="h-14 w-auto" src="/Img/logo.png" alt="Logo" />
+        <img className="h-10 w-auto" src="/Img/logo.png" alt="Logo" />
         <span className="text-xl font-bold">Biofield Power</span>
       </div>
 
