e8355c85cfc1c05a14da9ac0bb3dbb893b8c4b44
Praveen Kumar Gupta <pg23403@gmail.com>
2025-01-25 22:20:32 +0530

    Few update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import Header from "@/components/home/Header/Header";
+// import Header from "@/components/home/Header/Header";
 import Home1 from "@/components/home/page";
 export default function Home() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Home1 />
     </div>
   );
