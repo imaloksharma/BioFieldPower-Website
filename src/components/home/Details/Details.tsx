fac458243298bb16175784121b52dd7849ef20d5
Praveen Kumar Gupta <pg23403@gmail.com>
2025-02-21 15:01:13 +0530

    Alignment fixed

--- src/components/home/Details/Details.tsx
+++ src/components/home/Details/Details.tsx
@@ -21,35 +21,36 @@ export default function AboutUs() {
     return (
       <section className="p-10 bg-white-100">
       
-      <div className="space-y-8 container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-0 items-center">
-      <a href="/about" className="block">
-        <h1 className="text-4xl font-bold text-green-800 text-start lg:text-left cursor-pointer hover:underline">
-          About Us
-        </h1>
-      </a>
-      <div>
-        <p className="text-lg text-gray-700 leading-relaxed">
-          Biofield Power Pvt. Ltd. is a pioneering startup incubated at IIT Ropar and part of the prestigious
-          Agriculture and Water Technology Development Hub established in 2021 by the Department of Science &
-          Technology, Government of India.With a mission to empower farmers and transform rural economies, we develop innovative technologies that
-          bridge the gap between traditional agriculture and modern advancements.
-        </p>
-      </div>
-    </div>
-
-    <div className="space-y-8 container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-8 lg:gap-0 items-center">
-      <a href="/mission" className="block">
-        <h1 className="text-4xl font-bold text-green-800 text-start lg:text-left cursor-pointer hover:underline">
-          Our Mission
-        </h1>
-      </a>
+      <div className="space-y-8 container mx-auto grid grid-cols-12 items-center">
+  <a href="/about" className="block col-span-3 md:col-span-4 lg:col-span-3">
+    <h1 className="text-4xl font-bold text-green-800 text-start lg:text-left cursor-pointer hover:text-green-700">
+      About Us
+    </h1>
+  </a>
+  <div className="col-span-9 md:col-span-9 lg:col-span-9">
     <p className="text-lg text-gray-700 leading-relaxed">
-      At Biofield Power, our mission is to improve farmers&apos; income while enabling the seamless adoption of
-      advanced technologies in farmlands. By focusing on sustainable practices and cutting-edge solutions, we
-      aim to digitalize rural economies and provide a robust platform for industries to achieve their Sustainable
-      Development Goals (SDGs).
+      Biofield Power Pvt. Ltd. is a pioneering startup incubated at IIT Ropar and part of the prestigious
+      Agriculture and Water Technology Development Hub established in 2021 by the Department of Science & 
+      Technology, Government of India. With a mission to empower farmers and transform rural economies, we develop 
+      innovative technologies that bridge the gap between traditional agriculture and modern advancements.
     </p>
   </div>
+</div>
+
+<div className="space-y-8 container mx-auto grid grid-cols-12 mt-8 items-center">
+  <a href="/mission" className="block col-span-3 md:col-span-3 lg:col-span-3">
+    <h1 className="text-4xl font-bold text-green-800 text-start lg:text-left cursor-pointer hover:text-green-700">
+      Our Mission
+    </h1>
+  </a>
+  <p className="text-lg text-gray-700 leading-relaxed col-span-9 md:col-span-9 lg:col-span-9">
+    At Biofield Power, our mission is to improve farmers&apos; income while enabling the seamless adoption of
+    advanced technologies in farmlands. By focusing on sustainable practices and cutting-edge solutions, we
+    aim to digitalize rural economies and provide a robust platform for industries to achieve their Sustainable
+    Development Goals (SDGs).
+  </p>
+</div>
+
 
           <div className="space-y-8 container mx-auto mt-14">
             <h1 className="text-4xl font-bold text-green-800">What We Do?</h1>
