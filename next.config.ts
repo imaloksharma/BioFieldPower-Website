0a1d1ede90b81f3314ab0bc17e895ae114979ff4
jalajk201 <jsingh11_be21@thapar.edu>
2025-03-12 17:06:30 +0530

    Eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during the build process
+  },
 };
 
 export default nextConfig;
