79cbf6c7e8b02e3b68373f20820f2f2e0d7fc925
Praveen Kumar Gupta <pg23403@gmail.com>
2025-03-21 12:43:45 +0530

    Saved local changes before pulling latest code

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    EMAIL_HOST: process.env.EMAIL_HOST,
-    EMAIL_PORT: process.env.EMAIL_PORT,
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASS: process.env.EMAIL_PASS,
-    EMAIL_FROM: process.env.EMAIL_FROM,
-  },
-  
+  /* config options here */
 };
 
 export default nextConfig;
