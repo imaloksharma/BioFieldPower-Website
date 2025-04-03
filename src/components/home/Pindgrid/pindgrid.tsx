61e9663e947f9b5614aa0bdcb626f8725a1ea482
jalajk201 <jsingh11_be21@thapar.edu>
2025-04-03 23:11:23 +0530

    Deploy ready

--- src/components/home/Pindgrid/pindgrid.tsx
+++ src/components/home/Pindgrid/pindgrid.tsx
@@ -1,18 +1,17 @@
 
-import Image from "next/image";
 
 const PindGrid = () => {
   return (
     <section className="relative flex flex-col justify-between items-center w-full px-4  md:px-12 md:py-4 bg-white">
       <div className="w-full flex justify-center mb-4 md:mb-8">
-        <Image
+        <img
           src="/Pindgrid.svg"
           className="w-[100%]  md:w-[100%] h-auto"
           alt="Pindgrid"
         />
       </div>
       <div className="w-full flex justify-center">
-        <Image
+        <img
           src="/Pindgrid_lower.svg"
           className="w-[100%]  md:w-[100%] h-auto"
           alt="Pindgrid Lower"
