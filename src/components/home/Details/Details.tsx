44ddbff5373c21c3940f46282eb6977776b4fa41
jalajk201 <jsingh11_be21@thapar.edu>
2025-04-19 14:38:17 +0530

    About us updated

--- src/components/home/Details/Details.tsx
+++ src/components/home/Details/Details.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Details = () => {
   return (
-    <section className="w-full bg-white text-black px-4 md:px-16 pt-6 pb-0 overflow-hidden">
-      <h2 className="text-3xl md:text-4xl font-semibold text-green-700 text-center mb-28">
+    <section className="w-full bg-white text-black px-4 md:px-16 pt-6 pb-0 overflow-hidden font-[Raleway]">
+      <h2 className="text-3xl md:text-4xl font-semibold text-green-700 text-center mb-12 md:mb-28">
         About Us
       </h2>
 
       {/* Mobile: Show image, hide content */}
-      <div className="block md:hidden mb-8">
+      <div className="block md:hidden mb-8 mt-[-30px]">
         <Image
           src="/aboutus_mobile.png"
           alt="About Us Mobile"
@@ -20,37 +20,46 @@ const Details = () => {
       </div>
 
       {/* Desktop/Tablet: Show original content */}
-      <div className="hidden md:flex flex-col items-center gap-12 relative min-h-0 h-[950px]">
+      <div className="hidden md:flex flex-col items-center gap-12 relative min-h-0 h-[1000px]">
         {/* Truck */}
         <div className="relative left-[-440px]">
-          <Image src="/truck.svg" alt="Truck" width={260} height={160} />
+          <Image src="/truck.svg" alt="Truck" width={360} height={360} />
         </div>
+
         {/* Arrow to farm */}
         <div className="relative left-[-10px] top-[-210px]">
           <Image src="/up_arrow.svg" alt="Arrow" width={254} height={364} />
         </div>
+
         {/* Farm */}
         <div className="relative left-[350px] top-[-240px]">
-          <Image src="/farm.svg" alt="Farm" width={540} height={540} />
+          <Image src="/farm.svg" alt="Farm" width={640} height={540} />
         </div>
+
         {/* Description */}
-        <div className="relative left-[-310px] bottom-[440px] w-full max-w-3xl text-justify text-gray-700 px-4">
-          <p className="text-sm md:text-base">
-            <strong>Biofield Power Pvt. Ltd.</strong> is a pioneering startup
-            incubated at IIT Ropar and part of the prestigious Agriculture and
-            Water Technology Development Hub established in 2021 by the
-            Department of Science & Technology, Government of India. With a
-            mission to empower farmers and transform rural economies, we develop
-            innovative technologies that bridge the gap between traditional
-            agriculture and modern advancements.
+        <div className="relative left-[-210px] bottom-[540px] w-full max-w-4xl text-gray-900 px-4">
+          <p
+            className="text-justify"
+            style={{
+              fontFamily: "Raleway",
+              fontWeight: 400,
+              fontSize: "24px",
+              lineHeight: "32px",
+              letterSpacing: "-0.5%",
+              textAlign: "justify",
+            }}
+          >
+            <strong>Biofield Power Pvt. Ltd.</strong> is a pioneering startup<br /> incubated at IIT Ropar and part of the prestigious <br />Agriculture and Water Technology Development<br /> Hub established in 2021 by the Department of <br /> Science & Technology, Government of India. With <br /> a mission to empower farmers and transform rural<br />  economies, we develop innovative technologies<br />  that bridge the gap between traditional <br /> agriculture and modern advancements.
           </p>
         </div>
+
         {/* Arrow to biogas */}
-        <div className="relative left-[260px] bottom-[380px]">
+        <div className="relative left-[260px] bottom-[580px]">
           <Image src="/low_arrow.svg" alt="Arrow" width={134} height={64} />
         </div>
+
         {/* Biogas */}
-        <div className="relative left-[-180px] bottom-[500px]">
+        <div className="relative left-[-180px] bottom-[710px]">
           <Image src="/biogas.svg" alt="Biogas" width={500} height={400} />
         </div>
       </div>
